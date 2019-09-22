cc4955596fb65998645c6d77eda1c495a04ab6ca
Suman Mukherjee <sumanmukherjee03@gmail.com>
2019-09-22 13:49:00 -0700

    Simple fibonacci example with channels, recursion and go routines

---
+++ fibonacci.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func fibonacci(num1 int64, num2 int64, limit int64, c chan int64) {
+	if num1 == 0 && num2 == 1 {
+		c <- num1
+		c <- num2
+		limit = limit - 2
+	}
+	if limit == 0 {
+		close(c)
+		return
+	}
+	next := num1 + num2
+	c <- next
+	limit = limit - 1
+	go fibonacci(num2, next, limit, c)
+}
+
+func main() {
+	var maxItrs, err = strconv.ParseInt(os.Args[len(os.Args)-1], 0, 0)
+	if err != nil {
+		panic(err)
+	}
+	numbers := make(chan int64, maxItrs)
+	go fibonacci(0, 1, maxItrs, numbers)
+	for x := range numbers {
+		fmt.Println(x)
+	}
+}
