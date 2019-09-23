66df021e7fdc0821ab8747b12f45cdde1349f08f
Suman Mukherjee <sumanmukherjee03@gmail.com>
2019-09-22 21:31:52 -0700

    Added an improvement for best case

--- removedupes.go
+++ removedupes.go
@@ -8,6 +8,10 @@ import (
 
 func main() {
 	str := os.Args[1]
+	if len(str) < 2 {
+		fmt.Println(str)
+		return
+	}
 	for i := 0; i < len(str); i++ {
 		if strings.Contains(str[0:i], string(str[i])) {
 			continue
