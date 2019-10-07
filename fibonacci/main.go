4ee18557de7e1e26dc304c30c164cbbfa39f4cb7
Suman Mukherjee <sumanmukherjee03@gmail.com>
2019-10-06 20:42:04 -0700

    Simpler implementation of fibonacci

--- fibonacci/main.go
+++ fibonacci/main.go
@@ -6,18 +6,13 @@ import (
 	"strconv"
 )
 
-func fibonacci(num1 int64, num2 int64, limit int64, c chan int64) {
-	if num1 == 0 && num2 == 1 {
-		c <- num1
-		c <- num2
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
 	}
-	if len(c) == int(limit) {
-		close(c)
-		return
-	}
-	next := num1 + num2
-	c <- next
-	go fibonacci(num2, next, limit, c)
+	close(c)
 }
 
 func main() {
@@ -25,8 +20,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	numbers := make(chan int64, maxItrs)
-	go fibonacci(0, 1, maxItrs, numbers)
+	numbers := make(chan int)
+	go fibonacci(int(maxItrs), numbers)
 	for x := range numbers {
 		fmt.Println(x)
 	}
