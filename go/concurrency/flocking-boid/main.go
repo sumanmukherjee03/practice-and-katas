01efef9807a0ffdf2e42b203edd577b5790a1176
Suman Mukherjee <sumanmukherjee03@gmail.com>
2020-12-29 12:10:42 -0800

    Added a bunch of comments to the boid in a box example

--- go/concurrency/flocking-boid/main.go
+++ go/concurrency/flocking-boid/main.go
@@ -88,7 +88,7 @@ func createBoid(bId int) {
 		id: bId,
 	}
 	boids[bId] = &b
-	boidMap[int(b.position.x)][int(b.position.y)] = b.id
+	boidMap[int(b.position.x)][int(b.position.y)] = b.id // Mark initial positon in the grid with the boid id
 	go b.start()
 }
 
@@ -109,12 +109,12 @@ func (b *Boid) start() {
 
 func (b *Boid) moveOne() {
 	acceleration := b.calculateAcceleration()
-	mutex.Lock() // Acquire a write lock here
-	b.velocity = b.velocity.AddVector(acceleration).Limit(-1, 1)
-	boidMap[int(b.position.x)][int(b.position.y)] = -1
-	b.position = b.position.AddVector(b.velocity)
-	boidMap[int(b.position.x)][int(b.position.y)] = b.id
-	mutex.Unlock() // Release read lock
+	mutex.Lock()                                                 // Acquire a write lock here because we are updating the state variable boidMap which is used to draw the graphics on the screen
+	b.velocity = b.velocity.AddVector(acceleration).Limit(-1, 1) // Calculate new velocity
+	boidMap[int(b.position.x)][int(b.position.y)] = -1           // Mark old position as empty in grid
+	b.position = b.position.AddVector(b.velocity)                // Update to new position based on velocity
+	boidMap[int(b.position.x)][int(b.position.y)] = b.id         // Update grid at new position with boid id
+	mutex.Unlock()                                               // Release writers lock
 }
 
 func (b *Boid) calculateAcceleration() Vector2D {
@@ -125,32 +125,32 @@ func (b *Boid) calculateAcceleration() Vector2D {
 	count := 0.0
 	accelerationVector := Vector2D{x: b.borderBounce(b.position.x, screenWidth), y: b.borderBounce(b.position.y, screenHeight)}
 	targetPositionVector, targetVelocityVector, separationVector := Vector2D{x: 0, y: 0}, Vector2D{x: 0, y: 0}, Vector2D{x: 0, y: 0}
-	mutex.RLock() // Acquire a read lock here
+	mutex.RLock() // Acquire a read lock here so that you can safely access the state variable boidMap to get the boid ids located in the view radius
 	for i := xLeft; i <= xRight; i++ {
 		for j := yBottom; j <= yTop; j++ {
-			bId := boidMap[int(i)][int(j)]
-			if bId >= 0 && bId != b.id {
-				if d := boids[bId].position.Distance(b.position); d < viewRadius {
-					count += 1
-					targetVelocityVector = targetVelocityVector.AddVector(boids[bId].velocity)
-					targetPositionVector = targetPositionVector.AddVector(boids[bId].position)
+			bId := boidMap[int(i)][int(j)] // Iterate over view square area (because circles are hard to operate on in grid systems) to find out the neighbouring boid ids
+			if bId >= 0 && bId != b.id {   // Only if not the same as the current boid, enter the conditional section
+				if d := boids[bId].position.Distance(b.position); d < viewRadius { // Calculate distance and then compare with view radius to handle the circular case
+					count += 1                                                                 // Keep count of neighbours because you need to find averages
+					targetVelocityVector = targetVelocityVector.AddVector(boids[bId].velocity) // Needed to find average target velocity for flocking
+					targetPositionVector = targetPositionVector.AddVector(boids[bId].position) // Needed to find average target position for flocking
 					temp := b.position.SubtractVector(boids[bId].position)
-					separationVector = separationVector.AddVector(temp.DivideScalar(d))
+					separationVector = separationVector.AddVector(temp.DivideScalar(d)) // Look up separation vector in vector calculus here - https://www.youtube.com/watch?v=KU1VNb5ls6g
 				}
 			}
 		}
 	}
-	mutex.RUnlock() // Release read lock
+	mutex.RUnlock() // Release read lock because we do not need to access the state variable boidMap any more
 	if count > 0 {
-		targetVelocityVector = targetVelocityVector.DivideScalar(count)
-		targetPositionVector = targetPositionVector.DivideScalar(count)
+		targetVelocityVector = targetVelocityVector.DivideScalar(count) // Get average target velocity
+		targetPositionVector = targetPositionVector.DivideScalar(count) // Get average position vector
 	}
-	accelerationAlignmentVector := targetVelocityVector.SubtractVector(b.velocity)
-	accelerationAlignmentVector = accelerationAlignmentVector.MultiplyScalar(adjustmentRate)
-	accelerationCohesionVector := targetPositionVector.SubtractVector(b.position)
-	accelerationCohesionVector = accelerationCohesionVector.MultiplyScalar(adjustmentRate)
-	accelerationSeparationVector := separationVector.MultiplyScalar(adjustmentRate)
-
+	accelerationAlignmentVector := targetVelocityVector.SubtractVector(b.velocity)           // Get acceleration required to align speed
+	accelerationAlignmentVector = accelerationAlignmentVector.MultiplyScalar(adjustmentRate) // Adjust with the adjustment rate to make it easy for viewing
+	accelerationCohesionVector := targetPositionVector.SubtractVector(b.position)            // Get acceleration required to align position, ie to bring cohesion in the flock
+	accelerationCohesionVector = accelerationCohesionVector.MultiplyScalar(adjustmentRate)   // Adjust with the adjustment rate to make it easy for viewing
+	accelerationSeparationVector := separationVector.MultiplyScalar(adjustmentRate)          // Adjust the separation vector with the adjustment rate to make it easy for viewing
+	// Finally add all the vectors - alignment vector, cohesion vector and the separation vector to get the final acceleration vector
 	accelerationVector = accelerationVector.AddVector(accelerationAlignmentVector)
 	accelerationVector = accelerationVector.AddVector(accelerationCohesionVector)
 	accelerationVector = accelerationVector.AddVector(accelerationSeparationVector)
@@ -158,9 +158,9 @@ func (b *Boid) calculateAcceleration() Vector2D {
 }
 
 func (b *Boid) borderBounce(pos, max float64) float64 {
-	if pos < viewRadius {
+	if pos < viewRadius { // If current position dimension is within the 0 - view radius range, then accelerate slower in that direction
 		return 1 / pos
-	} else if pos > max-viewRadius {
+	} else if pos > max-viewRadius { // Similarly if current position dimension is within the view radius - screen limit range, then accelerate slower in that direction
 		return 1 / (pos - max)
 	}
 	return 0
