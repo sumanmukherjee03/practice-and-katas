38ed2b0903523130ed020a606e07fe3466099e54
Suman Mukherjee <sumanmukherjee03@gmail.com>
2021-01-10 11:10:01 -0800

    Context with value example

---
+++ go/concurrency/context-with-value/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type database map[string]bool
+type userIdKeyType string // We need a user defined data type for the context key
+
+var db = database{
+	"foo": true,
+}
+
+func main() {
+	describe()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	processRequest(ctx, "foo") // It is a go idiom to pass ctx as the first argument
+}
+
+func processRequest(ctx context.Context, userid string) {
+	newCtx := context.WithValue(ctx, userIdKeyType("userid"), userid)
+	ch := checkMembership(newCtx)
+	status := <-ch
+	fmt.Printf("Membership of userid %s is : %v\n", userid, status)
+}
+
+func checkMembership(ctx context.Context) <-chan bool {
+	ch := make(chan bool)
+	go func() {
+		defer close(ch)
+		k := ctx.Value(userIdKeyType("userid")).(string)
+		status := db[k]
+		ch <- status
+	}()
+	return ch
+}
+
+func describe() {
+	str := `
+This is an example of a cancellable context with value.
+Context with values are used as data bags to carry data from the context of a request to the goroutines downstream.
+
+
+_____________________
+	`
+	fmt.Println(str)
+}
