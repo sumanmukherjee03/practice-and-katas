990394626d0cdc53b682e48c15a05ba3d3c7163f
Suman Mukherjee <sumanmukherjee03@gmail.com>
2021-02-03 07:08:51 -0800

    Slight better error handling by moving it to a utility

--- go/rest-api-project/bookstore_users-api/domain/users/user_dao.go
+++ go/rest-api-project/bookstore_users-api/domain/users/user_dao.go
@@ -1,13 +1,10 @@
 package users
 
 import (
-	"fmt"
-	"strings"
-
-	"github.com/go-sql-driver/mysql"
 	"github.com/sumanmukherjee03/practice-and-katas/go/rest-api-project/bookstore_users-api/datasources/mysql/usersdb"
 	"github.com/sumanmukherjee03/practice-and-katas/go/rest-api-project/bookstore_users-api/utils/date_utils"
 	"github.com/sumanmukherjee03/practice-and-katas/go/rest-api-project/bookstore_users-api/utils/errors"
+	"github.com/sumanmukherjee03/practice-and-katas/go/rest-api-project/bookstore_users-api/utils/mysql_utils"
 )
 
 var (
@@ -15,7 +12,6 @@ var (
 )
 
 const (
-	sqlErrorNoRows  = "no rows in result set"
 	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
 	queryGetUser    = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?;"
 )
@@ -35,10 +31,7 @@ func (u *User) Get() *errors.RestErr {
 	// However, if we used stmt.Query, it would have returned *Rows in which case, we would have
 	// had a need to defer rows.Close() so that we dont end up with idle connections to the DB.
 	if getErr := stmt.QueryRow(u.Id).Scan(&u.Id, &u.FirstName, &u.LastName, &u.Email, &u.DateCreated); getErr != nil {
-		if strings.Contains(getErr.Error(), sqlErrorNoRows) {
-			return errors.NewNotFoundError(fmt.Errorf("Could not find an user with the given id : %d", u.Id))
-		}
-		return errors.NewInternalServerError(getErr)
+		return mysql_utils.ParseError(getErr)
 	}
 	return nil
 }
@@ -52,15 +45,7 @@ func (u *User) Save() *errors.RestErr {
 	u.DateCreated = date_utils.GetNowString()
 	insertRes, insertErr := stmt.Exec(u.FirstName, u.LastName, u.Email, u.DateCreated)
 	if insertErr != nil {
-		sqlErr, ok := insertErr.(*mysql.MySQLError)
-		if !ok {
-			return errors.NewInternalServerError(fmt.Errorf("Could not convert error returned by DB into a mysql error : %v", insertErr))
-		}
-		switch sqlErr.Number {
-		case 1062: // Represents a Duplicate Key error when inserting in a database
-			return errors.NewBadRequestError(fmt.Errorf("Email for user already exists : %s", u.Email))
-		}
-		return errors.NewInternalServerError(insertErr)
+		return mysql_utils.ParseError(insertErr)
 	}
 	userId, lastInsertIdErr := insertRes.LastInsertId() // Get the id of the row just inserted
 	if lastInsertIdErr != nil {
---
+++ go/rest-api-project/bookstore_users-api/utils/mysql_utils/mysql_utils.go
@@ -0,0 +1,28 @@
+package mysql_utils
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/sumanmukherjee03/practice-and-katas/go/rest-api-project/bookstore_users-api/utils/errors"
+)
+
+const (
+	sqlErrorNoRows = "no rows in result set"
+)
+
+func ParseError(err error) *errors.RestErr {
+	sqlErr, ok := err.(*mysql.MySQLError)
+	if !ok {
+		if strings.Contains(err.Error(), sqlErrorNoRows) {
+			return errors.NewNotFoundError(fmt.Errorf("Could not find record with the given id - %v", err))
+		}
+		return errors.NewInternalServerError(fmt.Errorf("Encountered an internal server error when parsing database error - %v", err))
+	}
+	switch sqlErr.Number {
+	case 1062: // Represents a Duplicate Key error when inserting in a database
+		return errors.NewBadRequestError(fmt.Errorf("Key for record in insert already exists - %v", err))
+	}
+	return errors.NewInternalServerError(fmt.Errorf("Encountered an internal server error when persisting to the database - %v", err))
+}
