adfd95d82c0d6d614089e17a7defc0a20d3ffee3
Suman Mukherjee <sumanmukherjee03@gmail.com>
2021-02-20 16:55:55 -0800

    Added some more tests for getting users

--- go/rest-api-project/bookstore_oauth-api/src/repository/rest/users_repository_test.go
+++ go/rest-api-project/bookstore_oauth-api/src/repository/rest/users_repository_test.go
@@ -1,12 +1,15 @@
 package rest
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 	"testing"
 
 	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/assert"
 	"github.com/sumanmukherjee03/practice-and-katas/go/rest-api-project/bookstore_oauth-api/src/domain/users"
+	"github.com/sumanmukherjee03/practice-and-katas/go/rest-api-project/bookstore_oauth-api/src/utils/errors"
 )
 
 // TestMain is the entrypoint of every test suite in a package is the TestMain function
@@ -29,19 +32,42 @@ func teardown() {
 func TestLoginUserSuccess(t *testing.T) {
 	assert := assert.New(t)
 	respUser := users.User{Id: 1, FirstName: "Foo", LastName: "Bar", Email: "foo.bar@baz.com"}
-	responder := httpmock.NewJsonResponderOrPanic(200, respUser)
+	responder := httpmock.NewJsonResponderOrPanic(http.StatusOK, respUser)
 	httpmock.RegisterResponder("POST", "http://localhost:8080/users/login", responder)
 	repo := usersRepository{}
-	res, err := repo.LoginUser("foo.bar@baz.com", "foobarbaz")
+	user, err := repo.LoginUser("foo.bar@baz.com", "foobarbaz")
 	assert.Nil(err, "error is expected to be nil")
-	assert.Equal("Foo", res.FirstName, "first name is incorrect")
+	assert.Equal("Foo", user.FirstName, "first name is incorrect")
 }
 
-// func TestLoginUserInvalidLoginCredentials(t *testing.T) {
-// }
+func TestLoginUserInvalidLoginCredentials(t *testing.T) {
+	assert := assert.New(t)
+	respErr := errors.NewNotFoundError(fmt.Errorf("Could not find user with username and password"))
+	responder := httpmock.NewJsonResponderOrPanic(respErr.Status, respErr)
+	httpmock.RegisterResponder("POST", "http://localhost:8080/users/login", responder)
+	repo := usersRepository{}
+	user, restErr := repo.LoginUser("foo.bar@baz.com", "foobarbaz")
+	assert.EqualValues(restErr.Error, "not_found", "should be a not found error")
+	assert.Nil(user, "user should be nil")
+}
 
-// func TestLoginUserInvalidUserJsonResponse(t *testing.T) {
-// }
+func TestLoginUserInternalServerError(t *testing.T) {
+	assert := assert.New(t)
+	respErr := errors.NewInternalServerError(fmt.Errorf("Could not read user from database"))
+	responder := httpmock.NewJsonResponderOrPanic(respErr.Status, respErr)
+	httpmock.RegisterResponder("POST", "http://localhost:8080/users/login", responder)
+	repo := usersRepository{}
+	user, restErr := repo.LoginUser("foo.bar@baz.com", "foobarbaz")
+	assert.EqualValues(restErr.Error, "internal_server_error", "should be an internal server error")
+	assert.Nil(user, "user should be nil")
+}
 
-// func TestLoginUserNoError(t *testing.T) {
-// }
+func TestLoginUserConnectionFailure(t *testing.T) {
+	assert := assert.New(t)
+	httpmock.RegisterResponder("POST", "http://localhost:8080/users/login", httpmock.ConnectionFailure)
+	repo := usersRepository{}
+	user, restErr := repo.LoginUser("foo.bar@baz.com", "foobarbaz")
+	assert.EqualValues(restErr.Error, "internal_server_error", "should be an internal server error")
+	assert.Contains(restErr.Message, "Encountered an error making downstream api call", "should be an internal server error")
+	assert.Nil(user, "user should be nil")
+}
