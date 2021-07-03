126c76b941302ad413c42f146d5728c6c94b3ef7
Suman Mukherjee <sumanmukherjee03@gmail.com>
2021-07-02 19:18:10 -0700

    Got an animation for conways game of life working

--- go/abstract_problems/conways_game_of_life/main.go
+++ go/abstract_problems/conways_game_of_life/main.go
@@ -9,48 +9,131 @@ But the cells' new value depends on these following rules :
   - In other cases the state of a cell stays the same
   - A cell can have 8 neighbours unless it is at the edge of the grid
 */
+
 package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"os/exec"
+	"runtime"
 	"strconv"
 	"time"
 )
 
-const (
-	gridSize = 41
-)
+type Game struct {
+	GridSize int
+	Grid     *[][]int
+}
 
 var (
-	grid *[][]int
+	game  *Game
+	clear map[string]func()
 )
+
+func init() {
+	clear = make(map[string]func())
+	commonClearFn := func() {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["linux"] = commonClearFn
+	clear["darwin"] = commonClearFn
+}
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	fmt.Println("Starting conways game of life")
-	initGrid()
-	printMatrix(grid)
+	x, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatalf("Could not parse input for grid size - %v", err)
+	}
+	gridSize := x + 2
+	grid := genMatrix(gridSize, func(p, q int) int {
+		return rand.Intn(100000) % 2
+	})
+	game := &Game{
+		GridSize: gridSize,
+		Grid:     grid,
+	}
+
+	ticker := time.NewTicker(3 * time.Second)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				Clear()
+				fmt.Print(game)
+				game.Step()
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	time.Sleep(60 * time.Second)
+	ticker.Stop()
+	done <- true
 }
 
-func initGrid() {
+func Clear() {
+	fn, ok := clear[runtime.GOOS]
+	if !ok {
+		log.Fatal("Clear function not supported for this OS")
+	}
+	fn()
+}
+
+func genMatrix(size int, fn func(int, int) int) *[][]int {
 	var m [][]int
-	for i := 0; i < gridSize; i++ {
-		r := make([]int, gridSize)
+	for i := 0; i < size; i++ {
+		r := make([]int, size)
 		for j, _ := range r {
-			r[j] = rand.Intn(100000) % 2
+			if i == 0 || i == size-1 || j == 0 || j == size-1 {
+				r[j] = 0
+			} else {
+				r[j] = fn(i, j)
+			}
 		}
 		m = append(m, r)
 	}
-	grid = &m
+	return &m
 }
 
-func printMatrix(m *[][]int) {
-	for i := 0; i < len((*m)); i++ {
-		str := ""
-		for j := 0; j < len((*m)[i]); j++ {
-			str += " " + strconv.Itoa((*grid)[i][j])
+func (g *Game) String() string {
+	str := ""
+	for i := 0; i < g.GridSize; i++ {
+		s := ""
+		for j := 0; j < g.GridSize; j++ {
+			s += " " + strconv.Itoa((*g.Grid)[i][j])
 		}
-		fmt.Println(str)
+		str += fmt.Sprintf("%s\n", s)
 	}
+	return str
+}
+
+func (g *Game) Step() {
+	nextValFn := func(i, j int) int {
+		sum := 0
+		for r := i - 1; r <= i+1; r++ {
+			for c := j - 1; c <= j+1; c++ {
+				if r != i && c != j {
+					sum += (*g.Grid)[r][c]
+				}
+			}
+		}
+		switch {
+		case sum == 3: // If 3 neighbours are alive the node becomes 1
+			return 1
+		case sum == 2: // If 2 neighbours are alive the node stays as is
+			return (*g.Grid)[i][j]
+		default: // In all other cases the node transitions to 0
+			return 0
+		}
+	}
+	g.Grid = genMatrix(g.GridSize, nextValFn)
 }
