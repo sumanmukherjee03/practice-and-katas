7ab1bac5139d907775661de1aab929b2a567e9ce
Suman Mukherjee <sumanmukherjee03@gmail.com>
2021-10-08 08:14:35 -0700

    Added the models for new tables that we added

--- go/vigilate/internal/models/models.go
+++ go/vigilate/internal/models/models.go
@@ -39,3 +39,42 @@ type Preference struct {
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 }
+
+// Host model
+type Host struct {
+	ID            int
+	HostName      string
+	CanonicalName string
+	URL           string
+	IP            string
+	IPV6          string
+	Location      string
+	OS            string
+	Active        string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
+// Service model
+type Service struct {
+	ID          int
+	ServiceName string
+	Icon        string
+	Active      string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+// HostService model
+type HostService struct {
+	ID             int
+	HostID         int
+	ServiceID      int
+	ScheduleNumber int
+	ScheduleUnit   string
+	Status         string
+	LastCheck      time.Time
+	Active         string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
