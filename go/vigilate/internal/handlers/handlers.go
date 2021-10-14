31b1f4701f20b93e861b4af3bfa5d804ca40ee06
Suman Mukherjee <sumanmukherjee03@gmail.com>
2021-10-14 08:22:55 -0700

    Getting the form in place for creating and updating hosts

--- go/vigilate/internal/handlers/handlers.go
+++ go/vigilate/internal/handlers/handlers.go
@@ -134,6 +134,11 @@ func (repo *DBRepo) Host(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// PostHost handles the create/update of a host
+func (repo *DBRepo) PostHost(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Posted form!"))
+}
+
 // AllUsers lists all admin users
 func (repo *DBRepo) AllUsers(w http.ResponseWriter, r *http.Request) {
 	vars := make(jet.VarMap)
