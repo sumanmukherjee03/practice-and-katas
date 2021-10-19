86a0fcfe64b8bca1f96a27292290d2141f38d6f5
Suman Mukherjee <sumanmukherjee03@gmail.com>
2021-10-19 07:22:45 -0700

    Better way to display the information of services assocaited with hosts

--- go/vigilate/internal/repository/dbrepo/hosts.go
+++ go/vigilate/internal/repository/dbrepo/hosts.go
@@ -73,6 +73,48 @@ func (m *postgresDBRepo) GetHostById(id int) (models.Host, error) {
 		return h, err
 	}
 
+	hostServicesStmt := `SELECT hs.id, hs.host_id, hs.service_id, hs.active, hs.schedule_number, hs.schedule_unit, hs.last_check, hs.status, hs.created_at, hs.updated_at,
+    s.id, s.service_name, s.active, s.icon, s.created_at, s.updated_at
+    FROM host_services hs
+    LEFT JOIN services s ON (s.id = hs.service_id)
+    WHERE host_id = $1`
+
+	rows, err := m.DB.QueryContext(ctx, hostServicesStmt, h.ID)
+	if err != nil {
+		log.Println(err)
+		return h, fmt.Errorf("Encountered error in populating HostServices for host - %d : %v", h.ID, err)
+	}
+	defer rows.Close()
+
+	var hostServices []models.HostService
+	for rows.Next() {
+		var hs models.HostService
+		err := rows.Scan(
+			&hs.ID,
+			&hs.HostID,
+			&hs.ServiceID,
+			&hs.Active,
+			&hs.ScheduleNumber,
+			&hs.ScheduleUnit,
+			&hs.LastCheck,
+			&hs.Status,
+			&hs.CreatedAt,
+			&hs.UpdatedAt,
+			&hs.Service.ID,
+			&hs.Service.ServiceName,
+			&hs.Service.Active,
+			&hs.Service.Icon,
+			&hs.Service.CreatedAt,
+			&hs.Service.UpdatedAt,
+		)
+		if err != nil {
+			log.Println(err)
+			return h, fmt.Errorf("Encountered error in populating HostServices for host - %d, host_service - %d : %v", h.ID, hs.ID, err)
+		}
+		hostServices = append(hostServices, hs)
+	}
+	h.HostServices = hostServices
+
 	return h, nil
 }
 
