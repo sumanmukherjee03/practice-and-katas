e696289c571c1e6a0a7e8beea182e33d440e4a87
Suman Mukherjee <sumanmukherjee03@gmail.com>
2021-10-28 08:56:18 -0700

    More advanced sql queries, particularly related to stored proc - conditions, case statements, loops etc

--- mysql/example_sql_queries.sql
+++ mysql/example_sql_queries.sql
@@ -120,7 +120,21 @@ select distinct cid into course_ids from enrolled;
 create table course_ids(select distinct cid from enrolled);
 
 --  In this case you insert into an existing table
+--  However, it is important to keep in mind that output redirection as inserts into another table may fail if the table has constraints
 insert into course_ids(select distinct cid from enrolled);
+
+
+
+
+
+--  ##############################################################################################
+--  #################################### FILTERING AND ORDERING ##################################
+--  ##############################################################################################
+
+--  This is going to produce different results each time because the order is not guaranteed
+select sid, name from students where login like '%@cs.org' limit 20 offset 20
+--  This is going to produce consistent results because the order is guaranteed unless ofcourse new rows get added
+select sid, name from students where login like '%@cs.org' order by sid asc, name asc limit 20 offset 20
 
 
 
@@ -131,6 +145,8 @@ insert into course_ids(select distinct cid from enrolled);
 --  ##############################################################################################
 
 --  Subqueries are queries within queries which can be used within where or from clauses
+--  The inner query can reference outputs from the outer query
+
 select emp_name, dept, salary
 from employees
 where salary > (select avg(salary) from employees);
@@ -143,12 +159,71 @@ select product_code, product_name, msrp from products
 where product_code in (select product_code from orderdetails where price_each > 20 and price_each < 100);
 
 
+--  Find all the sudent names that are enrolled in courses with ids between 15 to 245
+select name from students where sid in (select sid from enrolled where cid between 15 and 245)
+--  Find all the courses that have no students enrolled in them
+select * from courses where not exists (select * from enrolled where courses.cid = enrolled.cid)
+
+
 
 
 
 --  ##############################################################################################
---  ############################################## UNION #########################################
+--  ############################################## JOINS #########################################
 --  ##############################################################################################
+
+--  Inner join is for finding intersection of 2 sets
+
+--  Find players that are registered both in cricket and football matches
+select c.match_id, c.player, f.match_id, f.player from cricket as c inner join football as f on c.player = f.player
+
+--  Here's another example of an inner join but using the `using` keyword
+select productcode, productname, textdescription from products inner join productlines using(productline);
+
+--  This is an example of 3 way inner join
+select o.ordernumber, o.status, p.productname, sum(od.quantityordered * od.priceofeachproduct) as revenue
+from orders as o inner join orderdetails as od on o.ordernumber = od.ordernumber
+inner join products as p on p.productcode = od.productcode
+group by ordernumber;
+
+--  A left join returns all the rows from the left table and the matching rows from the right table
+
+--  In this example below there will be some rows where ordernumber and status will be empty
+--  because those customers have no orders
+select c.customernumber, c.customername, o.ordernumber, o.status from customers as c left join orders as o on c.customernumber = o.customernumber;
+
+--  Using the information above, this query finds customers who have not placed any orders
+select c.customernumber, c.customername, o.ordernumber, o.status from customers as c left join orders as o on c.customernumber = o.customernumber where ordernumber is null;
+
+
+--  A right join returns all the rows from the right table and the matching rows from the left table
+
+--  The example below has all values selected from employees table which is the right table.
+--  There will be rows where customername and customer phone will be null.
+--  This means not all employees have sold stuff to customers as sales reps
+select c.customername, c.phone, e.employeenumber, e.email
+from customers as c right join employees as e on e.employeenumber = c.salesrepnumber
+order by employeenumber;
+
+
+--  A self join is a join of a table with itself
+
+--  This result set returns a manager with multiple employees reporting to him/her.
+select concat(m.lastname, ', ', m.firstname) as manager, concat(e.lastname, ', ', e.firstname) as employee
+from employees as e inner join employees as m
+on m.employee_number = e.reports_to
+order by manager;
+
+
+--  Full outer join
+--  If a flavour of sql does not support full outer join, the best way to achieve it is
+--  by combining the result sets of a left outer join and right outer join via the union operator
+select c.customername, o.ordernumber from customers as c
+left join orders as o on c.customernumber = o.customernumber
+union
+select c.customername, o.ordernumber from customers as c
+right join orders as o on c.customernumber = o.customernumber
+
 
 --  When 2 tables have the same structure for the columns you select, and you want to join the rows, ie
 --  you want rows from both the tables show up in 1 result, you use UNION.
@@ -165,10 +240,34 @@ select SalesOrderId, OrderDate, SubTotal, SalesPersonId from sales.StoreSales
 
 
 
+
 --  ##############################################################################################
---  ############################################ RECURSION #######################################
+--  ###################################### COMMON TABLE EXPRESSIONS ##############################
 --  ##############################################################################################
---  Recursive query
+
+--  CTE is an alternative to temp tables and views. It provides a temp table to work with for the lifetime of a query
+--  You can also bind output columns to names for use in the select clause
+with cteName (col1, col2) as
+(
+  select 1, 2
+) select col1 + col2 from cteName
+
+--  Find student with the highest id that is enrolled in at least one course
+with cteSource (maxId) as (
+  select max(sid) from enrolled
+) select name from students, cteSource where students.sid = cteSource.maxId
+
+
+-- Simple example of a recursive query with CTE that produces an output of numbers from 1 to 10
+--  The recursive keyword here may be required in postgres and may not be required in mysql
+with recursive cteSource (counter) as
+(
+  (select 1)
+  union all
+  (select counter + 1 from cteSource where counter < 10)
+) select * from cteSource;
+
+--  A more complex recursive query
 --  ParentEmployeeKey here represents the employee key of the manager
 --  If the ParentEmployeeKey of someone is NULL, it means he/she is the CEO or Boss
 with EmployeeHierarchy as
@@ -196,6 +295,12 @@ with EmployeeHierarchy as
 --  ##############################################################################################
 --  ####################################### STORED PROCEDURES ####################################
 --  ##############################################################################################
+
+--  List stored procs
+show procedure status like '%pattern%'
+
+--  Drop stored procs
+drop procedure if exists abc;
 
 --  Example of a simple procedure
 delimiter &&
@@ -244,6 +349,110 @@ call count_employees(@F_emps);
 select @F_emps as female_emps;
 
 
+--  You can also modify a variable passed to the stored proc using the INOUT parameter type
+delimiter $$
+create procedure SetCounter(INOUT counter int, IN inc int)
+begin
+  set counter = counter + inc;
+end$$
+delimiter ;
+
+set @counter = 1; -- set the counter variable to 1
+call SetCounter(@couter, 3);
+select @counter; -- 4
+
+
+--  You can use conditions inside stored procs.
+--  The IF statement has three forms: simple IF-THEN statement, IF-THEN-ELSE statement, and IF-THEN-ELSEIF-ELSE statement.
+delimiter $$
+create procedure customer_level(IN customer_id int, OUT customer_level varchar(20))
+begin
+  declare credit decimal(10, 2) default 0;
+  select credit_limit into credit from customers where id = customer_id;
+  if credit > 50000 then
+    set customer_level = 'platinum';
+  elseif credit > 10000 and credit <= 50000 then
+    set customer_level = 'gold';
+  else
+    set customer_level = 'silver';
+  end if;
+end$$
+delimiter ;
+
+call customer_level(11, @level);
+select @level;
+
+
+--  Stored procs support case statements
+delimiter $$
+create procedure get_shipping_time(IN customer_id int, shipping_info varchar(50))
+begin
+  declare customer_country varchar(100);
+  select country into customer_country from customers where id = customer_id;
+  case customer_country
+  when 'US' then
+    set shipping_info = '2-day shipping';
+  when 'CA' then
+    set shipping_info = '3-day shipping';
+  else
+    set shipping_info = '5-day shipping';
+  end case;
+end$$
+delimiter ;
+
+call get_shipping_time(9, @shipping);
+select @shipping;
+
+
+--  In case you dont want to throw any error if no cases match you can have an empty begin+end in the else clause
+--  The case statements can also come with multiple conditions instead of just matching
+delimiter $$
+create procedure get_delivery_status(IN customer_id int, OUT delivery_status varchar(100))
+begin
+  declare waiting_days int default 0;
+  select datediff(est_delivery_date, shipped_date) into waiting_days from orders where customer_id = customer_id;
+  case
+  when waiting_days = 0 then
+    set delivery_status = 'ontime';
+  when waiting_days >= 1 and waiting_days < 3 then
+    set delivery_status = 'arriving';
+  when waiting_days >= 3 and waiting_days < 5 then
+    set delivery_status = 'late';
+  when waiting_days >= 5 and waiting_days < 10 then
+    set delivery_status = 'verylate';
+  else
+    set delivery_status = 'lost';
+end$$
+delimiter ;
+
+call get_delivery_status(7, @delivery);
+select @delivery;
+
+--  This is an example of looping inside a procedure
+delimiter $$;
+create procedure example_loop;
+begin
+  declare x int;
+  declare str varchar(100);
+  set x = 1;
+  set str = '';
+  --  Here the loop is labeled as looper
+  looper: LOOP
+    if x > 10 then
+      LEAVE looper;
+    end if;
+    set x = x + 1;
+    if (x mod 2) then
+      iterate looper;
+    else
+      set str = concat(str, x, ',');
+    end if;
+  end LOOP;
+  select str;
+end$$
+delimiter ;
+
+call example_loop();
 
 
 --  ##############################################################################################
@@ -339,6 +548,14 @@ select st_id, st_name, row_number() over (partition by st_id, st_name order by s
 --  This example will rank the rows based on the value of field1
 --  So, if multiple rows have the same value for field1, they will have the same rank
 select field1, rank() over (order by field1) as test_rank from demo;
+
+--  Find the student with the highest grade for each course
+--  The inner query here groups tuples by cid and then sorts a group by grade
+--  The ranking table is a temp table that exists only for the lifetime of this query
+select * from (
+  select *, rank() over (partition by cid order by grade asc) as rank from enrolled
+) as ranking
+where ranking.rank = 1;
 
 
 --  The first_value function gets the first entry for emp_name when the employees are ordered by salary.
