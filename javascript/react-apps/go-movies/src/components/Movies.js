d83b0154298c017f4c49bec9edfefec99076ca4b
Suman Mukherjee <sumanmukherjee03@gmail.com>
2021-12-26 17:28:22 -0800

    A little better error handling in the frontend code

--- javascript/react-apps/go-movies/src/components/Movies.js
+++ javascript/react-apps/go-movies/src/components/Movies.js
@@ -2,7 +2,11 @@ import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Movies extends Component {
-  state = {movies: [], isLoaded: false};
+  state = {
+    movies: [],
+    isLoaded: false,
+    error: null
+  };
 
   // The componentDidMount function is part of the react life cycle.
   // It trigger after an instance of the React component has been mounted into the DOM, ie after the component has rendered on the DOM.
@@ -12,38 +16,60 @@ export default class Movies extends Component {
   // There are some other lifecycle methods - componentDidMount, componentDidUpdate, componentWillUnmount
   componentDidMount() {
     fetch("http://localhost:4000/v1/movies")
-      .then((resp) => (resp.json()))
-      .then(data => {
+      .then((response) => {
+        let status = parseInt(response.status);
+        if (status >= 400) {
+          let err = Error;
+          err.message = "Encountered an error with status code - " + status;
+          this.setState({error: err});
+        }
+        return response.json();
+      })
+      .then((data) => {
         this.setState({
           movies: data.movies,
-          isLoaded: true,
-        })
+          isLoaded: true
+        });
+      }, (error) => {
+        this.setState({
+          error: error,
+          isLoaded: true
+        });
       });
   }
 
   render() {
     // Easy way to multi assign values from map
-    const {movies, isLoaded} = this.state;
+    const {movies, isLoaded, error} = this.state;
+
     if (!isLoaded) {
       return (
         <Fragment>
-          <p>Loading</p>
+          <p>Loading...</p>
         </Fragment>
       );
     } else {
-      return (
-        <Fragment>
-          <h2>Choose a movie</h2>
-          <ul>
-            {movies.map((m) => (
-              <li key={m.id}>
-                {/* Note the syntax of javascript templating here */}
-                <Link to={`/movies/${m.id}`}>{m.title}</Link>
-              </li>
-            ))}
-          </ul>
-        </Fragment>
-      );
+      if (!error) {
+        return (
+          <Fragment>
+            <h2>Choose a movie</h2>
+            <ul>
+              {movies.map((m) => (
+                <li key={m.id}>
+                  {/* Note the syntax of javascript templating here */}
+                  <Link to={`/movies/${m.id}`}>{m.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </Fragment>
+        );
+      } else {
+        return (
+          <Fragment>
+            <p>{error.message}</p>
+          </Fragment>
+        );
+      }
     }
   }
 }
