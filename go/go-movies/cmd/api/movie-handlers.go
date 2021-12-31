b2a7600d51278f70da5c5728aeb57debda6e7e5a
Suman Mukherjee <sumanmukherjee03@gmail.com>
2021-12-31 08:08:45 -0800

    Got the insertion of movies working with the backend

--- go/go-movies/cmd/api/movie-handlers.go
+++ go/go-movies/cmd/api/movie-handlers.go
@@ -1,8 +1,26 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
+	"time"
+
+	"go-movies/models"
 )
+
+// Dedidated type to handle marshaling and unmarshaling of movie payloads coming from the frontend as json
+type MoviePayload struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Year        string `json:"year"`
+	ReleaseDate string `json:"release_date"`
+	Runtime     string `json:"runtime"`
+	Rating      string `json:"rating"`
+	MPAARating  string `json:"mpaa_rating"`
+}
 
 func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 	id, err := app.getIdFromUrlParams(w, r)
@@ -34,13 +52,67 @@ func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
-	type editMovieJsonResp struct {
-		OK bool `json:"ok"`
+	var payload MoviePayload
+
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		app.badRequestErrorJSON(w, err)
+		return
 	}
 
-	movie := editMovieJsonResp{
-		OK: true,
+	var movie models.Movie
+
+	if len(payload.ID) > 0 {
+		movieID, err := strconv.Atoi(payload.ID)
+		if err != nil {
+			app.badRequestErrorJSON(w, err)
+			return
+		}
+		movie.ID = movieID
 	}
+
+	if len(payload.Runtime) > 0 {
+		rt, err := strconv.Atoi(payload.Runtime)
+		if err != nil {
+			app.badRequestErrorJSON(w, err)
+			return
+		}
+		movie.Runtime = rt
+	}
+
+	if len(payload.Rating) > 0 {
+		r, err := strconv.Atoi(payload.Rating)
+		if err != nil {
+			app.badRequestErrorJSON(w, err)
+			return
+		}
+		movie.Rating = r
+	}
+
+	if len(payload.ReleaseDate) > 0 {
+		rd, err := time.Parse("2006-01-02", payload.ReleaseDate)
+		if err != nil {
+			app.badRequestErrorJSON(w, err)
+			return
+		}
+		movie.ReleaseDate = rd
+		movie.Year = movie.ReleaseDate.Year()
+	}
+
+	movie.Title = payload.Title
+	movie.Description = payload.Description
+	movie.MPAARating = payload.MPAARating
+
+	if movie.ID == 0 {
+		newID, err := app.models.DB.InsertMovie(movie)
+		if err != nil {
+			app.serverErrorJSON(w, err)
+			return
+		}
+		movie.ID = newID
+	} else {
+		fmt.Println("Update movie here")
+	}
+
 	if err := app.writeJSON(w, http.StatusOK, movie, "movie"); err != nil {
 		app.serverErrorJSON(w, err)
 		return
