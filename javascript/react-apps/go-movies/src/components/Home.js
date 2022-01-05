8f8fc15f77a8710dcb1ddd83d05571178ffe3634
Suman Mukherjee <sumanmukherjee03@gmail.com>
2022-01-05 07:19:16 -0800

    Example of including an image in React app

--- javascript/react-apps/go-movies/src/components/Home.js
+++ javascript/react-apps/go-movies/src/components/Home.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import Ticket from "./../images/movie_tickets.jpg";
+import "./Home.css";
 
 export default class Home extends Component {
   render() {
     return (
-      <h2>Home</h2>
+      <div className="text-center">
+        <h2>Home</h2>
+        <hr />
+        <img src={Ticket} alt="movie ticket" />
+        <hr />
+        <div className="ticket"></div>
+      </div>
     );
   }
 }
