dc9017ee34be2b0b81192a6d3e808d8e424a72a8
Suman Mukherjee <sumanmukherjee03@gmail.com>
2022-01-19 08:14:29 -0800

    Easy 2 pointers example problem

---
+++ python/grokking-coking-interview/two-pointers/README.md
@@ -0,0 +1,4 @@
+## Description
+
+In problems that have sorted arrays (not linked lists) and we need to find a subset of elements that
+fulfill certain constraints, this approach is useful.
---
+++ python/grokking-coking-interview/two-pointers/pair-with-sum/main.py
@@ -0,0 +1,34 @@
+def describe():
+    desc = """
+Problem: Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target.
+Example:
+    Input: [1, 2, 3, 4, 6], target=6
+    Output: [1, 3]
+    Explanation: The numbers at index 1 and 3 add up to 6: 2+4=6
+
+------------
+    """
+    print(desc)
+
+def two_sum(arr, k):
+    head = 0
+    tail = len(arr) - 1
+    while head < tail:
+        s = arr[head] + arr[tail]
+        if s == k:
+            return [head, tail]
+        elif s < k:
+            head += 1
+        else:
+            tail -= 1
+    return [-1, -1]
+
+def main():
+    describe()
+    arr = [1,2,3,4,6]
+    k = 6
+    res = two_sum(arr, k)
+    print("Input : ", arr, k)
+    print("Resulting indices are : ", res)
+
+main()
