3685b45ff369a971d01d9f6aceed04f4c9c06b03
Suman Mukherjee <sumanmukherjee03@gmail.com>
2022-02-02 07:00:15 -0800

    Binary search on sorted array - better improved solution

--- python/grokking-coking-interview/binary-search/order-agnostic-binary-search/main.py
+++ python/grokking-coking-interview/binary-search/order-agnostic-binary-search/main.py
@@ -50,27 +50,23 @@ def recursive_binary_search(arr, key, start = 0, stop = -1):
 
 #  Iterative binary search
 def binary_search(arr, key):
-  start, end = 0, len(arr) - 1
-  isAscending = arr[start] < arr[end]
-  while start <= end:
-    # calculate the middle of the current range
-    mid = start + (end - start) // 2
-
-    if key == arr[mid]:
-      return mid
-
-    if isAscending:  # ascending order
-      if key < arr[mid]:
-        end = mid - 1  # the 'key' can be in the first half
-      else:  # key > arr[mid]
-        start = mid + 1  # the 'key' can be in the second half
-    else:  # descending order
-      if key > arr[mid]:
-        end = mid - 1  # the 'key' can be in the first half
-      else:  # key < arr[mid]
-        start = mid + 1  # the 'key' can be in the second half
-
-  return -1  # element not found
+    start, end = 0, len(arr)-1
+    is_ascending = arr[start] < arr[end]
+    while start <= end:
+        mid = start + (end - start)//2
+        if key == arr[mid]:
+            return mid
+        if is_ascending:
+            if key < arr[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        else:
+            if key < arr[mid]:
+                start = mid + 1
+            else:
+                end = mid - 1
+    return -1
 
 
 
