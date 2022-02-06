16080aa2898cec8203cb47441b0d8e11f1f65db2
Suman Mukherjee <sumanmukherjee03@gmail.com>
2022-02-05 16:28:05 -0800

    Some more examples of advanced mysql concepts

---
+++ mysql/cascade_delete.sql
@@ -0,0 +1,25 @@
+--  ##############################################################################################
+--  ########################################## CASCADE DELETE ####################################
+--  ##############################################################################################
+
+CREATE TABLE buildings(
+  building_no INT PRIMARY KEY AUTO_INCREMENT,
+  building_name VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL
+)
+
+--  The ON DELETE CASCADE clause at the end is what causes the rooms to get deleted when the building is deleted
+CREATE TABLE rooms (
+  room_no INT PRIMARY KEY AUTO_INCREMENT,
+  room_name VARCHAR(255) NOT NULL,
+  building_no INT NOT NULL,
+  FOREIGN KEY (building_no) REFERENCES buildings (building_no) ON DELETE CASCADE
+)
+
+
+--  As an admin this is a tip to find out which table is affected by ON DELETE CASCADE referential action
+USE information_schema;
+SELECT table_name FROM referential_constraints
+  WHERE constraint_schema = 'buildings_db'
+  AND referenced_table_name = 'buildings'
+  AND delete_rule = 'CASCADE'
--- mysql/joins.sql
+++ mysql/joins.sql
@@ -17,6 +17,7 @@ inner join products as p on p.productcode = od.productcode
 group by ordernumber;
 
 --  A left join returns all the rows from the left table and the matching rows from the right table
+--  A left join in a select statement is used to find rows in the left table that have or dont have matching rows in the right table.
 
 --  In this example below there will be some rows where ordernumber and status will be empty
 --  because those customers have no orders
@@ -65,3 +66,30 @@ select SalesOrderId, OrderDate, SubTotal from sales.StoreSales
 --  If the columns dont match you can add a placeholder blank/empty column
 select SalesOrderId, OrderDate, SubTotal, SalesPersonId from sales.StoreSales
   union select SalesOrderId, OrderDate, SubTotal, '' from sales.EcommSales;
+
+
+
+
+--  ### ------------------------------------------------------------------------------------------------------ ###
+--  ### ------------------------------------------------------------------------------------------------------ ###
+--  ### ------------------------------------------------------------------------------------------------------ ###
+
+--  DELETE with INNER JOIN to delete data from multiple tables
+--  This will delete row from t1 with id 1 and also rows from t2 where t1_id is 1
+DELETE t1, t2 FROM t1 INNER JOIN t2 ON t2.t1_id = t1.id WHERE t1.id = 11;
+
+
+--  A left join in a DELETE statement can be used to delete rows in the left table that DONT HAVE matching rows in the right table.
+--  This is an example of deleting from rows from t1 that do not have corresponding rows in t2.
+--  Important to note that we only put t1 after the DELETE clause and not both t1, t2.
+DELETE t1 FROM t1 LEFT JOIN t2 on t1.id = t2.t1_id WHERE t2.t1_id is NULL;
+DELETE customers FROM customers LEFT JOIN orders ON customers.customerNumber = orders.customerNumber WHERE orderNumber IS NULL;
+
+
+--  UPDATE with INNER JOIN
+--  Here we only specify employees after the UPDATE clause because we only want to update the employees table
+--  since there is no where clause here we update all rows of the employees table
+UPDATE employees INNER JOIN merits ON employees.performance = merits.performance SET salary = salary + salary * percentage;
+
+--  This is an example of an UPDATE LEFT JOIN to update rows in employees table where there is no corresponding row in the merits table
+UPDATE employees LEFT JOIN merits ON employees.performance = merits.performance SET salary = salary + salary * 0.015 WHERE merits.performance is NULL;
